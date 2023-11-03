3821c82bbf67130ff5476dd14a2a549ac1443e7b
Brehhhhh <114107186+Brehhhhh@users.noreply.github.com>
2023-11-03 09:37:05 -0400

    commit fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp/BudgetTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp/BudgetTeleOp.java
@@ -8,7 +8,10 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 
 import org.firstinspires.ftc.teamcode.Hardware.HardwareBudgetRobot;
 
@@ -19,8 +22,19 @@ public class BudgetTeleOp extends LinearOpMode {
     HardwareBudgetRobot robot = new HardwareBudgetRobot(this);
     static final int maxHeight = -2150;
 
+    private Servo servo1;
+
+    private DcMotor arm;
+
     @Override
     public void runOpMode() {
+        servo1 = hardwareMap.servo.get("Servo1");  // replace "your_servo_name" with the actual name in your configuration
+        arm = hardwareMap.dcMotor.get("Arm");
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("Status", "Initialization Complete");
+
         robot.init();
 
 
@@ -56,6 +70,49 @@ public class BudgetTeleOp extends LinearOpMode {
             robot.motor2.setPower(backLeftPower * throtte_control * slowDown);
             robot.motor3.setPower(frontRightPower * throtte_control * slowDown);
             robot.motor4.setPower(backRightPower * throtte_control * slowDown);
+
+            // Servo Code
+            if (gamepad1.right_bumper) {
+                servo1.setPosition(0.0);
+                telemetry.addData("Status", "Rotating Servo Clockwise");
+            } else {
+                servo1.setPosition(0.5);
+                telemetry.addData("Status", "Stopping Servo");
+            }
+
+            if (gamepad1.left_bumper) {
+                servo1.setPosition(1.0);
+                telemetry.addData("Status", "Rotating Servo Clockwise");
+            } else {
+                servo1.setPosition(0.5);
+                telemetry.addData("Status", "Stopping Servo");
+            }
+
+            if (gamepad1.dpad_up) {
+                servo1.setPosition(1.0);
+                telemetry.addData("Status", "Rotating Servo Clockwise");
+            }
+            if (gamepad1.dpad_down) {
+                servo1.setPosition(0.5);
+                telemetry.addData("Status", "Rotating Servo Clockwise");
+            }
+
+
+            //Arm code
+            double power = 1;
+            if (gamepad1.a) {
+                arm.setTargetPosition(0);
+                arm.setPower(power);
+                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            } else if (gamepad1.left_stick_y * -1 > 0) {
+                arm.setTargetPosition(arm.getCurrentPosition() + 300);
+                arm.setPower(power);
+                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            } else if (gamepad1.left_stick_y * -1 < 0) {
+                arm.setTargetPosition(arm.getCurrentPosition() - 300);
+                arm.setPower(power);
+                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
 
         }
     }
